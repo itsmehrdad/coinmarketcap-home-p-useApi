b81756180f425cbd36336b6b1c210eecbd0fa527
Mehrdad <108585974+itsmehrdad@users.noreply.github.com>
2022-09-19 18:04:01 +0430

    Update top-ten-crypto.js

--- src/api/top-ten-crypto.js
+++ src/api/top-ten-crypto.js
@@ -1,24 +1,3 @@
-// const axios = require('axios')
-
-// export default function handler (resolve, reject){
-  
-//    const getData =  async () => {
-    
-//    const response = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${'3cd3c5e4-2b5e-43cb-8189-05fe5795ab03'}`, {
-    
-//       headers: {
-//         'Access-Control-Allow-Origin'  : '*' , // 'Accept': '*/*',// 'Access-Control-Allow-Methods' : 'GET',// 'Content-Type' : 'application/x-www-form-urlencoded'
-//       }
-//     })
-//     const data = await response.data.data
-//     console.log(data)
- 
-//   }
-//   getData()
-  
-// };
-
-
 const axios = require('axios');
 
 let response = null;
